8103982455fbb7eda0e233689d2dedf1172886ba
hyyu <hyyu713@gmail.com>
2022-11-05 21:38:12 +0800

    add file

---
+++ products.py
@@ -0,0 +1,12 @@
+product = []
+goods = []
+while True:
+	name = input('請輸入商品名稱:')
+	if name == 'q':
+		break
+	price = input('請輸入商品價格:')
+	goods.append([name, price])
+print(goods)
+print(goods[0][1])
+for good in goods:
+	print('商品名稱{}商品價格{}'.format(good[0], good[1]))
