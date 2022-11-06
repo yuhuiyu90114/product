4e04b4bac7add8eb2ecd7f224061904c2979c2ec
hyyu <hyyu713@gmail.com>
2022-11-06 21:25:11 +0800

    ninth version

--- products.py
+++ products.py
@@ -1,29 +1,33 @@
+def read_file(filename):
+	goods = []
 
-goods = []
-
-import os
-if os.path.isfile('products.csv'):
-	print('找到檔案!')
-	with open ('products.csv', 'r') as f:
-		for line in f:
-			if '商品,價格' in line:
-				continue
-			name, price = line.strip().split(',')
-			goods.append([name, price])
+	import os
+	if os.path.isfile(filename):
+		print('找到檔案!')
+		with open (filename, 'r') as f:
+			for line in f:
+				if '商品,價格' in line:
+					continue
+				name, price = line.strip().split(',')
+				goods.append([name, price])
+		print(goods)
+	else:
+		print('找不到檔案!')
+	return goods
+def input_file(goods):
+	while True:
+		name = input('請輸入商品名稱:')
+		if name == 'q':
+			break
+		price = input('請輸入商品價格:')
+		goods.append([name, price])
 	print(goods)
-else:
-	print('找不到檔案!')
-
-while True:
-	name = input('請輸入商品名稱:')
-	if name == 'q':
-		break
-	price = input('請輸入商品價格:')
-	goods.append([name, price])
-print(goods)
-print(goods[0][1])
-for good in goods:
-	print('商品名稱{}商品價格{}'.format(good[0], good[1]))
+	print(goods[0][1])
+	return goods
+def print_file(goods):
+	
+	for good in goods:
+		print('商品名稱{}商品價格{}'.format(good[0], good[1]))
 
 #product = []
 #article = []
@@ -31,7 +35,13 @@ for good in goods:
 #article.append(price)
 #product.append(article)
 
-with open('products.csv', 'w') as f:
-	f.write('商品,價格\n')	
-	for good in goods:
-		f.write(good[0] + ',' + good[1] + '\n')
+def write_file(filename, goods):
+	with open(filename, 'w') as f:
+		f.write('商品,價格\n')	
+		for good in goods:
+			f.write(good[0] + ',' + good[1] + '\n')
+
+goods = read_file('products.csv')
+goods = input_file(goods)
+print_file(goods)
+write_file('products.csv', goods)
