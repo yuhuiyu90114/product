d909337f4851a8e1bd1971ee0f303bc8833da780
hyyu <hyyu713@gmail.com>
2022-11-06 21:52:30 +0800

    11th version

--- products.py
+++ products.py
@@ -1,19 +1,16 @@
 def read_file(filename):
-	goods = []
-
-	import os
-	if os.path.isfile(filename):
-		print('找到檔案!')
-		with open (filename, 'r') as f:
-			for line in f:
-				if '商品,價格' in line:
-					continue
-				name, price = line.strip().split(',')
-				goods.append([name, price])
-		print(goods)
-	else:
-		print('找不到檔案!')
+	goods = []		
+	with open (filename, 'r') as f:
+		for line in f:
+			if '商品,價格' in line:
+				continue
+			name, price = line.strip().split(',')
+			goods.append([name, price])
+	print(goods)	
 	return goods
+
+
+
 def input_file(goods):
 	while True:
 		name = input('請輸入商品名稱:')
@@ -40,8 +37,17 @@ def write_file(filename, goods):
 		f.write('商品,價格\n')	
 		for good in goods:
 			f.write(good[0] + ',' + good[1] + '\n')
+def main(filename):
+	import os
+	if os.path.isfile(filename):
+		print('找到檔案!')
+		goods = read_file(filename)		
+	else:
+		print('找不到檔案!')
 
-goods = read_file('products.csv')
-goods = input_file(goods)
-print_file(goods)
-write_file('products.csv', goods)
+
+	goods = input_file(goods)
+	print_file(goods)
+	write_file(filename, goods)
+
+main('products.csv')
